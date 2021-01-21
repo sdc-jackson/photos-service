e68d6d2899025756c9a8a0d78b6983075db63390
Manish Makin <sunnymakin@gmail.com>
2021-01-21 16:50:23 -0600

    create db seeding script

---
+++ database/seed.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/airbnb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const Photo = mongoose.model('Photo', {
+  room_id: String,
+  name: String,
+  photo_id: String,
+  caption: String,
+  is_primary: Boolean,
+  storage_url: String
+});
+
+Photo.deleteMany({}).then(() => {
+  console.log('deleted all photos..');
+  for (let i = 100; i < 200; i++) {
+    let isPrimary = true;
+    for (let j = 100; j < 110; j++) {
+      let propertyPhoto = new Photo({
+        room_id: i,
+        name: 'bedroom' + j,
+        photo_id: i.toString() + j.toString(),
+        caption: j + 'cozy' + i,
+        is_primary: isPrimary,
+        storage_url: ''
+      });
+      propertyPhoto.save().then(() => {
+        console.log('saved in db..');
+      });
+      isPrimary = false;
+    }
+  }
+});
---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = 5001;
+
+app.listen(PORT, () => {
+  console.log('server started at port: ', PORT);
+});
