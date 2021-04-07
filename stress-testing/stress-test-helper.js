2072545fac72b109a4276a957aac29dbd2aa34a8
jrozal <68182930+jrozal@users.noreply.github.com>
2021-04-06 23:09:48 -0700

    Complete local stress testing

--- stress-testing/stress-test-helper.js
+++ stress-testing/stress-test-helper.js
@@ -8,19 +8,19 @@ const randomNumberGenerator = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// get random id between 1 - 10M
+// get random id between 9M - 10M (last 10% of dataset)
 const getRandomId = (userContext, events, done) => {
-  const id = randomNumberGenerator(1, 10000000);
+  const id = randomNumberGenerator(9000000, 10000000);
   userContext.vars.id = id;
   return done();
 };
 
-// post random photo data to database
+// post random photo data to database (within last 10% of dataset)
 const postRandomData = (userContext, events, done) => {
   const photoNum = randomNumberGenerator(1, 1000);
 
   const photoId = uuidv4();
-  const roomNumber = randomNumberGenerator(1, 10000000);
+  const roomNumber = randomNumberGenerator(9000000, 10000000);
   const name = faker.commerce.productAdjective();
   const caption = faker.commerce.productName();
   const isPrimary = false;
