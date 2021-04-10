07e5ac15451a385664deca3c99ff3bafc11005d4
jrozal <68182930+jrozal@users.noreply.github.com>
2021-04-09 23:16:58 -0700

    Remove mongodb files

--- database/spec/index.test.js
+++
@@ -1,6 +0,0 @@
-const db = require('../index.js');
-
-test('should return data for a valid room id', async () => {
-  const data = await db.getPhotosByRoomId('100');
-  expect(photos.length).toBe(10);
-});
